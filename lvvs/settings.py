35f75265352f498b4d058b1707864d5257b3f117
John Cheng <jcheng727@gmail.com>
2014-11-11 17:47:33 -0600

    changed static_root

--- lvvs/settings.py
+++ lvvs/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = '/root/webapps/lvvs/lvvs/static-files/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../staticfiles/')
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
