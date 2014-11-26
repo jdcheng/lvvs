408f421dbecbf2bb4bf5efb86e21b76d22c8a724
John Cheng <jcheng727@gmail.com>
2014-11-26 10:24:50 -0600

    changed default page to welcome page

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^index/$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^thanks/$', views.toballot, name='toballot'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
     url(r'^options/submit/$', views.submit_options, name='submit_options'),
     url(r'^(?P<question_id>\d+)/options/$', views.options, name='options'),
     url(r'^newsurvey/$', views.get_new_survey, name='new_survey'),
-    url(r'^welcome/$', views.welcome, name='welcome'),
+    url(r'^$', views.welcome, name='welcome'),
 )
