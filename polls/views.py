c974a646eec67165adac41912a0f75befa54fd71
Patrick Dunphy <pdunphy@rice.edu>
2014-11-26 19:47:17 -0600

    removed vote button on ballot

--- polls/views.py
+++ polls/views.py
@@ -143,11 +143,13 @@ def vote(request, question_id):
         all_questions[question_id] = request.POST['choice']
         request.session['all_questions'] = all_questions
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "You didn't select a choice.",
-        })
+        # Go to the next race; a no vote
+        p = get_object_or_404(Question, pk=question_id)
+        return HttpResponseRedirect(reverse('polls:detail', args=(p.id+1,)))
+        #return render(request, 'polls/detail.html', {
+        #    'question': p,
+        #    'error_message': "You didn't select a choice.",
+        #})
     else:
         selected_choice.votes += 1
         selected_choice.save()
@@ -158,7 +160,7 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:detail', args=(p.id+1,)))
+    return HttpResponseRedirect(reverse('polls:detail', args=(p.id+1,)))
 
 def forward(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
