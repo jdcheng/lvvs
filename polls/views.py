a3f69bdfd418253e8a1394c2051f3a8fa16b9b06
Patrick Dunphy <pdunphy@rice.edu>
2014-12-02 00:47:51 -0600

    put options before help

--- polls/views.py
+++ polls/views.py
@@ -205,12 +205,17 @@ def toballot(request):
 
 def welcome(request):
     if request.method == 'POST':
-        return render(request, 'polls/options_initial.html')
+        return render(request, 'polls/help.html')
+        #return render(request, 'polls/options_initial.html')
     return render(request, 'polls/welcome.html')
+
+def help(request):
+    return render(request, 'polls/help.html')
 
 def options_initial(request):
     if request.method == 'POST':
-        return render(request, 'polls/help.html')
+        return render(request, 'polls/options_initial.html')
+        #return render(request, 'polls/help.html')
     return render(request, 'polls/welcome.html')
 
 def options(request, question_id):
