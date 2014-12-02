ed6a7e5009d6bfd7f0ba1d6e74e15e3587de3770
John Cheng <jcheng727@gmail.com>
2014-12-02 02:24:06 -0600

    fixed up survey

--- static/website_static/js/newsurvey.js
+++ static/website_static/js/newsurvey.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
     if (sessionStorage.getItem('color') != 'null') {
-      $("body").css('color', sessionStorage.getItem('color'));
+        $(".container").css('color', sessionStorage.getItem('color'));
     }
 
-    if (sessionStorage.getItem('background-color-color') != 'null') {
-      $("body").css('background-color', sessionStorage.getItem('background-color'));
+    if (sessionStorage.getItem('background-color') != 'null') {
+        $(".container").css('background-color', sessionStorage.getItem('background-color'));
     }
 
-    // alert("" + sessionStorage.getItem('size') + "")
-
-    if (sessionStorage.getItem('size') != 'null'){
-      $("body").css('fontSize', sessionStorage.getItem('size') + "px")
+    if (sessionStorage.getItem('size') != 'null') {
+        $("#ballot-container *").css('font-size', sessionStorage.getItem('size') + "px")
     }
 
-      //alert("" + sessionStorage.getItem('boldness') + "")
-
-    if (sessionStorage.getItem('boldness') != 'null'){
-      $("body").css('fontWeight', sessionStorage.getItem('boldness'))
+    if (sessionStorage.getItem('boldness') != 'null') {
+        $("#ballot-container *").css('fontWeight', sessionStorage.getItem('boldness'))
     }
 
     $("#ballot-container *").css('line-height', "1.125em");
+    $("#ballot-container *").css('list-style', "none");
+
 });
